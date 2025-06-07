95554c3cc39315f195bd3c6fa4b40b4c8d587ff2
kakuraccho <kakurattyo1216@gmail.com>
2025-06-07 15:58:43 +0900

    example file

---
+++ discordbot.py
@@ -0,0 +1,30 @@
+# 外部ライブラリからのimport
+import discord
+import os
+
+from dotenv import load_dotenv
+load_dotenv
+
+# botトークンを.envから読み込む
+TOKEN = os.getenv('TOKEN')
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がbotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
