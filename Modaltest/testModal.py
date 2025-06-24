caf026d455bfd794dfad466f143dcbbb306e9922
kakuraccho <kakurattyo1216@gmail.com>
2025-06-24 22:57:00 +0900

    addenv

--- Modaltest/testModal.py
+++ Modaltest/testModal.py
@@ -1,11 +1,6 @@
 import discord
-from discord import app_commands
 from discord.ext import commands
-import os
-from dotenv import load_dotenv
-load_dotenv()
 
-TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
