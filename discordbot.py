606bd040d90b26504bf35886426a2e5aeb6cac06
kakuraccho <kakurattyo1216@gmail.com>
2025-06-25 23:48:03 +0900

    lower

--- discordbot.py
+++ discordbot.py
@@ -26,7 +26,7 @@ class MyBot(discord.Client):
 
 bot = MyBot()
 
-@bot.tree.command(name="meigenSub")
+@bot.tree.command(name="meigen")
 async def submit(interaction: discord.Interaction):
     await interaction.response.send_message("名言(英文)を入力してください")
 
