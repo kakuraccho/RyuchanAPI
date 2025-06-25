23f09e41771d5eba4487693ba84743bfa0003e5b
kakuraccho <kakurattyo1216@gmail.com>
2025-06-25 23:46:53 +0900

    waitfor

--- discordbot.py
+++ discordbot.py
@@ -20,28 +20,29 @@ class MyBot(discord.Client):
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
         self.user_states = {}
+    
     async def setup_hook(self):
         await self.tree.sync()
 
 bot = MyBot()
 
-@bot.tree.command(name="submit", description="名言(英文)")
+@bot.tree.command(name="meigenSub")
 async def submit(interaction: discord.Interaction):
     await interaction.response.send_message("名言(英文)を入力してください")
-    bot.user_states[interaction.user.id] = "awaiting_input"
 
-@bot.event
-async def on_message(message: discord.Message):
-    if message.author.bot:
-        return
-    if bot.user_states.get(message.author.id) == "awaiting_input":
-        supabase.table("meigen").insert({"meigen_eng": message.content}).execute()
-        await message.channel.send("名言を保存しました")
-        bot.user_states[message.author.id] = None
-    await bot.process_commands(message)
+    def check(m):
+        return m.author.id == interaction.user.id and m.channle == interaction.channel
+    
+    try:
+        message = await bot.wait_for("message", check=check, timeout=60.0)
+        supabase.table("meigen").insert({"meinge_eng": message.content}).execute()
+        await interaction.followup.send("名言を保存しました")
+    except asyncio.TimeoutError:
+        await interaction.followup.send("時間切れです。もういちど送信してください")
+
 
 @bot.event
 async def on_ready():
-    print(f"Logge in as {bot.user}")
+    print(f"Logged in as {bot.user}")
 
 bot.run(DISCORD_BOT_TOKEN)
