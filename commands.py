fc8ecd900e4bb68bb9b0718a80b093b1cf32cdc1
kakuraccho <kakurattyo1216@gmail.com>
2025-06-27 21:44:55 +0900

    add ()

--- commands.py
+++ commands.py
@@ -9,7 +9,7 @@ async def save_meigen_to_db(supabase_client, text, user_id, username, guild_id):
             'user_id': str(user_id),
             'username': username,
             'guild_id': str(guild_id)
-        }).execute
+        }).execute()
 
         return True, None
     
