cee14bc9345660a9e2030e26d968224c733c450c
kakuraccho <kakurattyo1216@gmail.com>
2025-07-01 21:13:36 +0900

    add servicekey

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ load_dotenv()
 DISCORD_BOT_TOKEN = os.getenv('DISCORD_TOKEN')
 SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_TOKEN = os.getenv('SUPABASE_TOKEN')
+SUPABASE_SERVICE_KEY = os.getenv('SUPABASE_SERVICE_KEY')
 GUILD_ID = int(os.getenv('GUILD_ID'))
 
 # ---環境変数のチェック---
@@ -16,7 +17,7 @@ if not all([DISCORD_BOT_TOKEN, SUPABASE_URL, SUPABASE_TOKEN, GUILD_ID]):
     raise ValueError("環境変数に異常があります")
 
 # supabaseクライアント
-supabase = create_client(SUPABASE_URL, SUPABASE_TOKEN)
+supabase = create_client(SUPABASE_URL, SUPABASE_TOKEN,SUPABASE_SERVICE_KEY)
 
 # イベントループの最適化
 if hasattr(asyncio, 'set_event_loop_policy'):
